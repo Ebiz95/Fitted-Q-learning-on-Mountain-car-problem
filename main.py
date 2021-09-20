d9e6cbd56663059f838abc326de77cfe010b8ab6
Ebaa Asaad <67460090+Ebiz95@users.noreply.github.com>
2021-09-20 09:58:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from Agent import AgentBilinear
 env_name = "MountainCarContinuous-v0"
 env = gym.make(env_name)
 
-num_examples = 400	# All number of examples to be tested
-max_action = 1		# All max actions to be tested
-max_iterations = 500		# Max number of steps allowed in the game
-render_video = True			# Allows the user to visualize the performance of the algorithm (Set to True only when
-							# testing one set of parameters.
+num_examples = 400	# Number of examples sampled from the game
+max_action = 1		# Max power the car is allowed to have
+max_iterations = 500	# Max number of steps allowed in the simulation
+render_video = True	# Allows the user to visualize the performance of the algorithm
 
 state = env.reset()
 agent = AgentBilinear(env, min_action=-max_action, max_action=max_action)
